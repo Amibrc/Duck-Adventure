3a02051e52d07ee47519e08f7bd62b0a91d2ab24
Amibrc <amibrcg@gmail.com>
2025-01-09 15:46:08 +0200

    deleted unneeded functions

--- tools.py
+++ tools.py
@@ -3,20 +3,6 @@ from json import load
 from config.paths import DISPLAY_CONFIG_PATH
 
 
-def calculate_scale_factor(screen_width, screen_height, new_screen_width, new_screen_height):
-    return min(new_screen_width / screen_width, new_screen_height / screen_height)
-
-
-def scale_animations(original_animation_frames, current_animation_frames, scale_factor):
-    for state, directions in original_animation_frames.items():
-        for direction, frames in directions.items():
-            if isinstance(frames, tuple):
-                current_animation_frames[state][direction] = tuple(pygame.transform.scale(frame, (frame.get_width() * scale_factor, frame.get_height() * scale_factor)) for frame in frames)
-            else:
-                current_animation_frames[state][direction] = pygame.transform.scale(frames, (frames.get_width() * scale_factor, frames.get_height() * scale_factor))
-    return current_animation_frames
-
-
 def scale_image_to_screen(image, width, height):
     scale_max = max(width / image.get_width(), height / image.get_height())
     return pygame.transform.scale(image, (int(image.get_width() * scale_max), int(image.get_height() * scale_max)))
@@ -68,7 +54,7 @@ def collision_with_objects(character_rect, objects):
                     "collision_sides": {
                         "right": collision_y and character_rect.right >= obj_rect.left and character_rect.right <= obj_rect.right,
                         "left": collision_y and character_rect.left <= obj_rect.right and character_rect.left >= obj_rect.left,
-                        "top": obj_rect.top < character_rect.top < obj_rect.bottom and character_rect.bottom > obj_rect.bottom,
+                        "top": obj_rect.top <= character_rect.top <= obj_rect.bottom and character_rect.bottom >= obj_rect.bottom,
                         "bottom": character_rect.bottom > obj_rect.top and character_rect.bottom < obj_rect.bottom
                     }
                 }
